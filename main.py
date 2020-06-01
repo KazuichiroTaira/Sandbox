bec4ab7cd368b5585cf2b8ab1e2841f5690ee23e
KazuichiroTaira <ktaira098@gmail.com>
2020-06-01 17:59:14 +0300

    First commit of practice

---
+++ main.py
@@ -0,0 +1,86 @@
+#Basics of Python Tutorial:
+# https://www.youtube.com/watch?v=UsCQXe1OHZk&list=PLQVvvaa0QuDe8XSftW-RAxdo6OmaeL85M&index=4&t=0s
+
+print('This is an example of print function')
+
+print("we're going to the store")
+
+print("Hi!")
+
+print('We\'re going to the store')
+
+print('Hi', 'there')
+
+print('Hi', 5)
+
+# print('Hi'+ 5)
+# print(5+'Hi')
+
+print('Hi '+str(5))
+
+print('8'+str(5))
+
+# print(int('8.5')+5)
+#8.5 is not integer, so use float
+
+print(float('8.5')+5)
+
+#   + - * /
+
+print(1+3)
+
+print(4*4)
+
+print(25/5)
+
+print(5/34)
+
+print(4**4)
+
+# Variables
+
+exampleVar = 55+32
+print(exampleVar)
+
+exampleVar = print('whoo')
+
+x,y= (3,5)
+
+print(x)
+print(y)
+
+#While loop
+
+condition = 1
+
+while condition < 10:
+    print(condition)
+    condition += 1
+   # condition = condition +1
+
+
+#while True:
+    #print('doing stuff')
+
+#for loop
+
+exampleList = [1,5,6,1,6,7,8,9,345,53,5]
+
+for eachNumber in exampleList:
+    print(eachNumber)
+    print('continue program')
+
+for x in range(1, 11):
+    print(x)
+
+# If statement:
+# https://www.youtube.com/watch?v=4u2ClNCtcgY&list=PLQVvvaa0QuDe8XSftW-RAxdo6OmaeL85M&index=8
+
+
+
+
+
+
+
+
+
